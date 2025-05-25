2b103827230f8980ed6b1f704a161bc7a56c7947
刘敏清 ch <2985861993@qq.com>
2025-05-25 23:50:36 +0800

    添加函数调用

--- main.cpp
+++ main.cpp
@@ -1,26 +1,8 @@
 ﻿#include "./FCT_Node/headers.h"
-using namespace v8;
-using namespace node;
-namespace FCT
-{
-}
-namespace FCT
-{
-    class NodeCallBack
-    {
-    protected:
-
-    public:
-
-    };
-}
-void Init()
-{
-    FCT::NodeCommon::Init();
-}
 using namespace FCT;
 using namespace std;
-int main() {
+int main()
+{
     cout << "please install must libary by using npm" << endl;
     NodeEnvironment env;
     env.addModulePath("./node_modules");
@@ -36,8 +18,7 @@ try {
     console.log(chalk.green('This text is green!'));
     console.log(chalk.blue.bold('This text is blue and bold!'));
 
-    console.log('Module search paths:');
-    console.log(module.paths);
+    console.log('Loaded modules:', Object.keys(require.cache));
 } catch (err) {
     console.error('Error loading npm modules:', err.message);
 }
@@ -45,6 +26,46 @@ try {
     };
     env.args(args);
     env.excuteArgs(std::vector<std::string>({}));
-    int ret = env.excute();
-    return ret;
+    env.setup();
+    //env.executeArg();
+    /*
+    env.execute(R"(
+    const lodash = require('lodash');
+    console.log('test');
+)");*/
+    env.execute(R"(
+    const lodash = require('lodash');
+    const chalk = require('chalk');
+    function isValidVersion(version) {
+        const versionPattern = /^\d+\.\d+\.\d+$/;
+        const isValid = versionPattern.test(version);
+        console.log(chalk.yellow(`版本号 ${version} 验证结果: ${isValid ? '有效' : '无效'}`));
+        return isValid;
+    }
+    function foo(){
+        console.log(chalk.blue.bold('hello from nodejs'));
+    }
+    try {
+    console.log('Lodash loaded successfully');
+    console.log('Random number from lodash:', lodash.random(1, 100));
+
+    console.log(chalk.green('This text is green!'));
+    console.log(chalk.blue.bold('This text is blue and bold!'));
+
+    console.log('Loaded modules:', Object.keys(require.cache));
+} catch (err) {
+    console.error('Error loading npm modules:', err.message);
+}
+)");
+    env.callFunction("foo");
+    if (env.callFunction<bool>("isValidVersion","0.1.0"))
+    {
+        cout << "Version is valid" << endl;
+    } else
+    {
+        cout << "Version is not valid" << endl;
+    }
+    Sleep(1000);
+    env.stop();
+    return 0;
 }
